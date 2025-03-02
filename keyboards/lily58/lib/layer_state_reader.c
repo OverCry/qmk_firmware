391feb6e13e3091d973e699fb911469362dd2f57
Wong <wcho400@aucklanduni.ac.nz>
2025-03-02 22:39:19 +1300

    Add settings for personal keyboard

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -1,4 +1,5 @@
 #include "action_layer.h"
+// #include "quantum.h"
 #include <stdio.h>
 
 #define L_BASE 0
@@ -13,20 +14,20 @@ const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Base");
+    snprintf(layer_state_str, sizeof(layer_state_str), "L:Def");
     break;
   case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+    snprintf(layer_state_str, sizeof(layer_state_str), "L:Rai");
     break;
   case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+    snprintf(layer_state_str, sizeof(layer_state_str), "L:Low");
     break;
   case L_ADJUST:
   case L_ADJUST_TRI:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+    snprintf(layer_state_str, sizeof(layer_state_str), "L:Adj");
     break;
   default:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%u", layer_state);
+    snprintf(layer_state_str, sizeof(layer_state_str), "L:Undef-%u", layer_state);
   }
 
   return layer_state_str;
